eb77c2951ef1cda83f3217060ba60b0cf1c0ff67
Dakota L Martinez <dakotaleemusic@gmail.com>
2020-09-21 18:48:01 -0700

    add posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  add_flash_types :info, :error, :warning
+  private 
+
+  def current_user 
+    User.find_by_id(session[:user_id])
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
     binding.pry
     if @user.save 
       session[:user_id] = @user.id
+      redirect_to posts_path
     else
       render :new
     end
